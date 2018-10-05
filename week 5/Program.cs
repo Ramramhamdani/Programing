3d6159f4141097892f1e12697e5b174d1f0e5ad7
Ramramhamdani <ahamdany12@gmail.com>
2018-10-05 13:43:36 +0200

    first one done

--- week 5/Program.cs
+++ week 5/Program.cs
@@ -10,34 +10,38 @@ namespace week_5
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[20];
-            int[] asnwers = new int[20];
-            Double sum = 0;
-            Double dif = 0;
             
+            int[] numbers = new int[20];
+            Double sum = 0;
+            Double diff = 0;
+            int num = 0;
+            int count = 0;
+            Random rnd = new Random();
             for (int i = 0; i < numbers.Length; i++)
             {
-                Random rnd = new Random(); 
-                numbers[i] = rnd.Next(0,200);
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    int numberTax = Math.Abs(numbers[i]);
-                    Console.WriteLine("Element {0} is {1}", i, numberTax);
-                    
-                }
-                sum = sum + numberTax;
+                numbers[i] = rnd.Next(0, 200);
             }
-            
-            Double ave = sum / 20;
-            Console.WriteLine("The average is: ", ave);
-            foreach (int num in numbers)
+            foreach (int number in numbers)
             {
-                Double Diff = ave - num;
-                Console.WriteLine("The differencce between the average and the element{0} is ", num);
+                
+                Console.WriteLine("Element {0} is {1}",count,number); 
+                count++;
+                sum = sum + number;
             }
-
-            Console.ReadKey();
+            Double ave = sum / 20;
+            Console.WriteLine();
+            Console.WriteLine("The average is:" + ave);
+            Console.WriteLine();
+            foreach (int item in numbers)
+            {
+                
+                diff = ave - item;
+                Console.WriteLine("the difference between the average and element {0} is {1:0.00}", num, Math.Abs(diff));
+                num++;
+            }
             
+            Console.ReadKey();
+
         }
     }
 }
