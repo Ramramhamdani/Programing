07f7c2e9b2a7bdbd3dcdcf0871629a23883f5252
Ramramhamdani <ahamdany12@gmail.com>
2018-10-14 23:26:11 +0200

    assignment week 6
    5.4

--- assignment 6/6.3/Program.cs
+++ assignment 6/6.3/Program.cs
@@ -10,6 +10,44 @@ namespace _6._3
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a year (0 is stop value): ");
+            int year = int.Parse(Console.ReadLine());
+            bool LeapYear = true;
+
+            while (year != 0)
+            {
+                LeapYear = IsLeapYear(year);
+                //if the year is dividable by 400 or 4 but not by 100 it's leap
+                if (LeapYear)
+                {
+                    Console.WriteLine("{0} is a leap year.", year);
+                }
+                // if the year is no divividable by 400 or 4 but dividable by 100 it's not leap 
+                else if (LeapYear==false)
+                {
+                    Console.WriteLine("{0} is not a leap year.", year);
+                }
+                
+                Console.Write("Enter a year (0 is stop value): ");
+                year = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("End of program");
+            Console.ReadKey();
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            bool LeapYear = true;
+            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+            {
+                LeapYear = true;
+            }
+            // if the year is no divividable by 400 or 4 but dividable by 100 it's not leap 
+            else if (year % 400 != 0 || year % 4 != 0 && year % 100 == 0)
+            {
+                LeapYear = false;
+            }
+            return LeapYear;
 
         }
     }
---
+++ assignment 6/6.4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a text> ");
+            string text = Console.ReadLine();
+            int nrOfFullStops = 0;
+            int nrOfCommas = 0;
+            int nrOfSemiColons = 0;
+            SearchText(text, out nrOfFullStops, out nrOfCommas, out nrOfSemiColons);
+            Console.WriteLine("Result: {0} full stops, {1} commas, {2} semicolons"
+                                     ,nrOfFullStops,nrOfCommas,nrOfSemiColons);
+            Console.ReadKey();
+        }
+        static void SearchText(string text, out int nrOfFullStops, out int nrOfCommas, out int nrOfSemiColons)
+        {
+            nrOfSemiColons = 0;
+            nrOfFullStops = 0;
+            nrOfCommas = 0;
+            string semi = ";";
+            string full = ".";
+            string comma = ",";
+            foreach (char c in text)
+            {
+                if (c == ';')
+                {
+                    nrOfSemiColons++;
+                }
+                else if (c=='.')
+                {
+                    nrOfFullStops++;
+                }
+                else if (c==',')
+                {
+                    nrOfCommas++;
+                }
+
+            }
+           
+
+        }
+
+    }
+}
