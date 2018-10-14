d63eb96109fc3594b8ff2a409837c42151c916fb
Ramramhamdani <ahamdany12@gmail.com>
2018-10-14 22:16:56 +0200

    assignment week 6
    first 2

---
+++ assignment 6/6.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1
+{
+   
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter a price: ");
+            float input = float.Parse(Console.ReadLine());
+            float VAT = VatCalculation(input);
+            float Total = TotalPriceCal(input,VAT);
+            Console.Write("price: {0:0.00}, VAT: {1:0.00}, total: {2:0.00}", input, VAT, Total);
+            Console.ReadKey();
+        }
+        static float VatCalculation(float price)
+        {
+            float vat = (price * 21)/100;
+            return vat;
+            
+        }
+        static float TotalPriceCal(float price, float vat)
+        {
+            float total = price + vat;
+            return total;
+        }
+    }
+}
---
+++ assignment 6/6.2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number(0 is stop value): ");
+            int number = int.Parse(Console.ReadLine());
+            bool IsPrime = true;
+           
+            while (number != 0)
+            {
+                IsPrime = Prime(number);
+                if (IsPrime&& number!=1)
+                {
+                    Console.WriteLine("{0} is a prime number.", number);
+                }
+                else if (number==1)
+                {
+                    Console.WriteLine("{0} is not a prime number.", number);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a prime number.", number);
+                }
+                Console.Write("Enter a number(0 is stop value): ");
+                number = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("End of program");
+            Console.ReadKey();
+        }
+        static bool Prime(int input)
+        {
+            bool IsPrime = true;
+            int i = 2;
+            while (i<input && IsPrime)
+            {
+                if ((input%i)==0)
+                {
+                    IsPrime = false;
+                }
+                else if (input==1)
+                {
+                    IsPrime = false;
+                }
+                else
+                {
+                    i = i + 1;
+                }
+
+            }
+            return IsPrime;
+        }
+    }
+}
